e4e63dc370e0f1d6505bd45dfe008e99c43f2afa
Kris Jones <kristop.jones@gmail.com>
2020-12-13 18:33:58 -0700

    final

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-let money = 0;
+let oinks = 0;
 let modifier = 0;
 let purchase = 0;
 
 let clickUpgrades = {
-    food: {
+    mud: {
         price: 5,
         quantity: 0,
         multiplier: 2,
         adder: 0
     },
-    water: {
+    pens: {
         price: 10,
         quantity: 0,
         multiplier: 3,
         adder: 0
     },
-    jobs: {
+    farmers: {
         price: 15,
         quantity: 0,
         multiplier: 2,
         adder: 0
     },
-    houses: {
+    factories: {
         price: 50,
         quantity: 0,
         multiplier: 2,
@@ -30,25 +30,19 @@ let clickUpgrades = {
 }
 
 let autoUpgrades = {
-    money: {
-        price: 150,
+    upOinks: {
+        price: 5,
         quantity: 0,
         multiplier: 1
     },
-    hospitals: {
-        price: 2000,
-        quantity: 0,
-        multiplier: 2
-    }
 
 }
 
 
-function earth() {
-    money += 1 + modifier
+function pig() {
 
-
-
+    oinks += 1 + modifier
+    document.getElementById("song").play()
 
     update()
 }
@@ -58,8 +52,8 @@ function buyUpgrades(type) {
 
 
     // money if greater than upgrades price can use to purchase upgrades.. money decreases by amount of upgrade price
-    if (money >= clickUpgrades[type].price) {
-        money -= clickUpgrades[type].price
+    if (oinks >= clickUpgrades[type].price) {
+        oinks -= clickUpgrades[type].price
         //price multiplies
         clickUpgrades[type].price = clickUpgrades[type].price * clickUpgrades[type].multiplier
         //upgrades continue adding as you purchase
@@ -67,32 +61,46 @@ function buyUpgrades(type) {
         clickUpgrades[type].quantity = clickUpgrades[type].quantity + clickUpgrades[type].adder
         //click counts stack with earth clicks
         modifier = clickUpgrades[type].quantity + modifier
-
+        //autoupgrades
 
     }
-
 
     update()
 }
 
+function buyAuto() {
+    autoUpgrades.upOinks.quantity++
+    update()
+}
+
+function collectAuto() {
+    oinks += autoUpgrades.upOinks.quantity * autoUpgrades.upOinks.multiplier
+    update()
+}
+
+function startInterval() {
+    collectionInterval = setInterval(collectAuto, 3000)
+}
 
 
 
 
 function update() {
     //update money count (+1) click of earth
-    document.getElementById('money').innerHTML = money.toString()
+    document.getElementById('oinks').innerHTML = oinks.toString()
     //updates price
-    document.getElementById('food').innerText = '$' + clickUpgrades.food.price
-    document.getElementById('water').innerText = '$' + clickUpgrades.water.price
-    document.getElementById('jobs').innerText = '$' + clickUpgrades.jobs.price
-    document.getElementById('houses').innerText = '$' + clickUpgrades.houses.price
+    document.getElementById('mud').innerText = '$' + clickUpgrades.mud.price
+    document.getElementById('pens').innerText = '$' + clickUpgrades.pens.price
+    document.getElementById('farmers').innerText = '$' + clickUpgrades.farmers.price
+    document.getElementById('factories').innerText = '$' + clickUpgrades.factories.price
     //updates quantity
-    document.getElementById('food-q').innerText = '+' + clickUpgrades.food.quantity
-    document.getElementById('water-q').innerText = '+' + clickUpgrades.water.quantity
-    document.getElementById('jobs-q').innerText = '+' + clickUpgrades.jobs.quantity
-    document.getElementById('houses-q').innerText = '+' + clickUpgrades.houses.quantity
+    document.getElementById('mud-q').innerText = '+' + clickUpgrades.mud.quantity
+    document.getElementById('pens-q').innerText = '+' + clickUpgrades.pens.quantity
+    document.getElementById('farmers-q').innerText = '+' + clickUpgrades.farmers.quantity
+    document.getElementById('factories-q').innerText = '+' + clickUpgrades.factories.quantity
 
+    document.getElementById('addoinks').innerHTML = autoUpgrades.upOinks.quantity
 }
 
+startInterval()
 update()
